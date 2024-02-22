e6ab18a9b3ff83dabda0218286bcc90acb6a023a
Santos.283553 <Santos.283553@LAB-A-PC19>
2024-02-23 07:50:49 +0800

    Add project files.

---
+++ ComputeAverageApp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        private static double gradess, ave, gradesss;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" Enter 5 grades seperated by new line: ");
+            for (int i = 0; i < 5; i++) {
+
+                
+                    double grades = Convert.ToDouble(Console.ReadLine());
+
+                    gradess += grades;
+
+                    ave = gradess / 5;
+                    gradesss = Math.Round(ave);
+
+                }
+                Console.Write(" the average is " + ave + " and round off to " + gradesss);
+
+            }
+        }
+    }
+        
+    
+
+
+    
+
+
